18b3470af4582b0718c9bb2b39a18ce5285cb1ba
Youssef Abdulaziz <azizy5203@gmail.com>
2024-11-04 17:37:35 +0200

    27.Function Return Types & "void"

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 "use strict";
-const x = 4;
-console.log(x);
+var Role;
+(function (Role) {
+    Role[Role["admin"] = 0] = "admin";
+    Role[Role["guest"] = 1] = "guest";
+    Role[Role["owner"] = 2] = "owner";
+})(Role || (Role = {}));
+var person = {
+    name: 'John',
+    age: 30,
+    city: 'New York',
+    role: Role.admin,
+    talent: "Flex"
+};
+console.log(person.role);
+var ff = 10;
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,23 @@
-const x: any = 4
-console.log(x)
+enum Role {
+    admin, guest, owner
+}
+
+const person: {
+    name: string;
+    age: number;
+    city: string;
+    role: number;
+    talent: 'Flex' | 'Dev' | 4
+} = {
+    name: 'John',
+    age: 30,
+    city: 'New York',
+    role: Role.admin,
+    talent: "Flex"
+}
+
+console.log(person.role)
+
+type dd = number | string
+
+const ff: dd = 10    
